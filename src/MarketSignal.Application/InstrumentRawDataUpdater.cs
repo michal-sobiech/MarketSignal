8ef4c41a0d64b53682c809c9704500f4959f815b
Michał Sobiech <124889302+michal-sobiech@users.noreply.github.com>
2026-04-07 10:35:12 +0200

    feat(Contracts): create InstrumentRawDataUpdater

---
+++ src/MarketSignal.Application/InstrumentRawDataUpdater.cs
@@ -0,0 +1,27 @@
+using MarketSignal.Contracts;
+
+using NodaTime;
+
+namespace MarketSignal.Application;
+
+public class InstrumentRawDataUpdater(
+    IInstrumentRawDataProvider instrumentRawDataProvider,
+    InstrumentRawDataService instrumentRawDataService
+) {
+
+    private readonly IInstrumentRawDataProvider _rawDataProvider = instrumentRawDataProvider;
+    private readonly InstrumentRawDataService _rawDataService = instrumentRawDataService;
+
+    public async Task UpdateInstrumentRawData() {
+        IEnumerable<InstrumentRawDataRow> newRows = await FetchNewRows();
+        await _rawDataService.SaveMany(newRows);
+    }
+
+    private async Task<IEnumerable<InstrumentRawDataRow>> FetchNewRows() {
+        Instant newestSavedRowTime = await _rawDataService.FetchNewestRowTime();
+        Instant now = SystemClock.Instance.GetCurrentInstant();
+
+        return _rawDataProvider.FetchRawData(newestSavedRowTime, now);
+    }
+
+}
