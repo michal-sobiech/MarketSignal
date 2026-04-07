2e4e996db5734238f6afefdd794cc903d5a5ee39
Michał Sobiech <124889302+michal-sobiech@users.noreply.github.com>
2026-04-07 20:39:49 +0200

    chore(Worker): add Redis init to Program.cs

--- src/MarketSignal.Worker/Program.cs
+++ src/MarketSignal.Worker/Program.cs
@@ -1,7 +1,21 @@
+using MarketSignal.Infrastructure.Job;
 using MarketSignal.Worker;
+
+using Microsoft.Extensions.Options;
+
+using StackExchange.Redis;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Services.Configure<RedisOptions>(builder.Configuration.GetSection("Redis"));
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(serviceProvider => {
+    var options = serviceProvider.GetRequiredService<IOptions<RedisOptions>>().Value;
+    return ConnectionMultiplexer.Connect(options.ConnectionString);
+});
+
+builder.Services.AddSingleton<RedisJobQueueConsumer>();
+builder.Services.AddSingleton<RedisJobStore>();
 
 var host = builder.Build();
 host.Run();
