6bb1107f1380fa741c0b19c725f5a8ca7c4c6226
Michał Sobiech <124889302+michal-sobiech@users.noreply.github.com>
2026-04-08 23:35:25 +0200

    chore(Api): imlement CalcIndicatorValues

--- src/MarketSignal.Api/InstrumentIndicator/InstrumentIndicatorController.cs
+++ src/MarketSignal.Api/InstrumentIndicator/InstrumentIndicatorController.cs
@@ -1,7 +1,13 @@
+using MarkeSignal.Infrastructure.Job;
+
 using MarketSignal.Api.Mappers;
 using MarketSignal.Application;
 using MarketSignal.Contracts.Indicator;
 using MarketSignal.Contracts.Indicator.Spec;
+using MarketSignal.Contracts.Instrument;
+using MarketSignal.Contracts.Job;
+using MarketSignal.Contracts.Job.Payload;
+using MarketSignal.Contracts.Job.Store;
 using MarketSignal.Contracts.MarketDataProvider;
 
 using Microsoft.AspNetCore.Mvc;
@@ -13,10 +19,14 @@ namespace MarketSignal.Api.InstrumentIndicator;
 [ApiController]
 [Route("/instruments/indicators")]
 public class InstrumentIndicatorController(
+    IJobQueueProducer jobQueueProducer,
+    IJobStore jobStore,
     IndicatorService indicatorService,
     KeyValuePairsStringParser keyValuePairsStringParser
 ) : ControllerBase {
 
+    private readonly IJobQueueProducer _jobQueueProducer = jobQueueProducer;
+    private readonly IJobStore _jobStore = jobStore;
     private readonly IndicatorService _indicatorService = indicatorService;
     private readonly KeyValuePairsStringParser _keyValuePairsStringParser = keyValuePairsStringParser;
 
@@ -46,6 +56,34 @@ public class InstrumentIndicatorController(
             fromInstant,
             toInstant);
         return Ok(rows);
+    }
+
+    [HttpPost("calculate-values")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<GetIndicatorValuesResponse> CalcIndicatorValues(
+        [FromQuery] string symbol,
+        [FromQuery] string mic,
+        [FromQuery] string dataProvider,
+        [FromQuery] string indicatorName,
+        [FromQuery] string indicatorArgs
+    ) {
+        MarketDataProviderKind dataProviderKind = MarketDataProviderKindMapper.FromString(dataProvider);
+        IndicatorKind indicatorKind = IndicatorKindMapper.FromString(indicatorName);
+
+        var indicatorArgsDict = _keyValuePairsStringParser.Parse(indicatorArgs);
+        IndicatorSpec indicatorSpec = IndicatorSpecFactory.Of(indicatorKind, indicatorArgsDict);
+
+        var instrumentIndicatorSpec = InstrumentIndicatorSpec.Of(symbol, mic, dataProviderKind, indicatorSpec);
+
+        Guid jobId = new();
+        var payload = new CalcIndicatorJobPayload(instrumentIndicatorSpec);
+        JobEntity jobEntity = JobEntity.CreateNew(jobId, payload);
+
+        _jobStore.Save(jobEntity);
+        _jobQueueProducer.EnqueueJob(jobId);
+
+        var response = new JobIdResponse(jobId);
+        return Ok(response);
     }
 
 }
