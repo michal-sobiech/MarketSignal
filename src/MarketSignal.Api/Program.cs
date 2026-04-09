113756fd9447cb3140a4db451e9d96a604fb9eb4
Michał Sobiech <124889302+michal-sobiech@users.noreply.github.com>
2026-04-09 21:33:36 +0200

    fix(Api): main app finally starts

--- src/MarketSignal.Api/Program.cs
+++ src/MarketSignal.Api/Program.cs
@@ -18,16 +18,20 @@ builder.Services.AddSingleton<MarketDbOptions>(_ => new MarketDbOptions {
     Password = Environment.GetEnvironmentVariable("MARKET_DB_PASSWORD") ?? throw new ArgumentException("MARKET_DB_PASSWORD is not set"),
 });
 
+
+// var marketDbOptions = builder.Services.BuildServiceProvider().GetRequiredService<MarketDbOptions>();
+// Console.WriteLine($"DB: Server={marketDbOptions.Host};Port={marketDbOptions.Port};Database={marketDbOptions.DbName};Uid={marketDbOptions.UserName};Pwd={marketDbOptions.Password};AllowPublicKeyRetrieval=True;");
+
 builder.Services.AddDbContext<MarketDbContext>((serviceProvider, options) => {
     var marketDbOptions = serviceProvider.GetRequiredService<MarketDbOptions>();
-    string connectionString = $"Server={marketDbOptions.Host};Port={marketDbOptions.Port};Database={marketDbOptions.DbName};User Id={marketDbOptions.UserName};Password={marketDbOptions.Password};AllowPublicKeyRetrieval=True;";
+    string connectionString = $"Server={marketDbOptions.Host};Port={marketDbOptions.Port};Database={marketDbOptions.DbName};Uid={marketDbOptions.UserName};Pwd={marketDbOptions.Password};AllowPublicKeyRetrieval=True;";
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
 // Redis
 builder.Services.AddSingleton<RedisOptions>(_ => new RedisOptions {
     Host = Environment.GetEnvironmentVariable("REDIS_HOST") ?? throw new ArgumentException("REDIS_HOST is not set"),
-    Port = int.Parse(Environment.GetEnvironmentVariable("REDIS_HOST") ?? throw new ArgumentException("REDIS_PORT is not set"))
+    Port = int.Parse(Environment.GetEnvironmentVariable("REDIS_PORT") ?? throw new ArgumentException("REDIS_PORT is not set"))
 });
 builder.Services.AddSingleton<IConnectionMultiplexer>(serviceProvider => {
     var redisOptions = serviceProvider.GetRequiredService<RedisOptions>();
