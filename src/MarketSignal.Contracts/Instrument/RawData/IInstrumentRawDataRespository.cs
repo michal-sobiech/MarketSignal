812e2db8bf0f8d5a41a1888aa9ac88c8f4fa1150
Michał Sobiech <124889302+michal-sobiech@users.noreply.github.com>
2026-04-10 11:35:47 +0200

    chore(Contracts): update IInstrumentRawDataRepository method signatures

--- src/MarketSignal.Contracts/Instrument/RawData/IInstrumentRawDataRespository.cs
+++ src/MarketSignal.Contracts/Instrument/RawData/IInstrumentRawDataRespository.cs
@@ -3,15 +3,15 @@ using NodaTime;
 namespace MarketSignal.Contracts.Instrument.RawData;
 
 public interface IInstrumentRawDataRepository {
-    public Task<Instant?> FetchNewestRowTime(InstrumentSpec instrumentSpec);
+    public Task<Instant?> FetchNewestRowTime(long instrumentSpecId);
 
     public Task SaveMany(
-        InstrumentSpec instrumentSpec,
+        long instrumentSpecId,
         IEnumerable<InstrumentRawDataRow> rows
     );
 
     public Task<IEnumerable<InstrumentRawDataRow>> FetchByTimeRange(
-        InstrumentSpec instrumentSpec,
+        long instrumentSpecId,
         Instant fromInclusive,
         Instant toInclusive
     );
